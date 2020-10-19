68e97399c3e426d3f384f182b08bc0ac8717a718
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-10-19 22:02:38 +0300

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="container">
+    <AddUser />
+    <Users />
   </div>
 </template>
 
+<script>
+import AddUser from '../src/components/AddUser'
+import Users from '../src/components/Users'
+export default {
+  name: 'App',
+  components: {
+    AddUser,
+    Users
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container {
+  max-width: 500px;
+  padding-top: 50px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
-import Vue from "vue";
-import Vuex from "vuex";
 
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import UsersModule from '../store/modules/users-module'
+
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    UsersModule
+  }
+})
